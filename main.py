5518e745a13d475cdff9ed6a915ab054a4e241df
tedhyu <tedhyu@yahoo.com>
2020-03-23 15:48:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pyspark.sql import SparkSession, functions as F
 from pyspark.sql.types import *
 
 session = SparkSession.builder.getOrCreate()
-sc = SparkContext.getOrCreate()
-spark = SparkSession(sc)
-df = spark.read.load('s3://commoncrawl/cc-index/table/cc-main/warc/')
-df.createOrReplaceTempView('ccindex')
+sqldf = session.read.format("csv").option("header", True).option("inferSchema", True).load("s3://athena-tedyu/bball/2020/03/23/9315090e-a014-4a7b-9560-d92d99f6cce6.csv")
+warc_recs = sqldf.select("url", "warc_filename", "warc_record_offset", "warc_record_length").rdd
+print warc_recs.collect()
